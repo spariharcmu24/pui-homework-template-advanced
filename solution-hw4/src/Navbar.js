cfc89285442d85742d480f71e6609f05b0e1ef13
sp2418 <sparihar@andrew.cmu.edu>
2024-09-29 18:49:28 -0400

    worked on cart functionality and other user interactions

--- solution-hw4/src/Navbar.js
+++ solution-hw4/src/Navbar.js
@@ -2,23 +2,50 @@ import React, { Component } from 'react';
 import './Navbar.css';
 
 class Navbar extends Component {
+  // A React state property called totalCost is created to store the totalCost value after it's calculated in calculateTotalCost
+  constructor(props) {
+    super(props);
+    this.state = {
+        totalCost: 0.00,
+    };    
+  }
+
+  // The method below calculates the total cost of all items that are ordered. The list of orders comes from this.props.listCart, which is passed in from index.js. 
+  calculateTotalCost = () => {
+    let listCart = this.props.listCart;
+    let newTotal = 0;
+    for (let i = 0; i < listCart.length; i++){
+      let currOrder = listCart[i]
+      let currentPrice = Number(currOrder["updatedPrice"])
+      newTotal += currentPrice
+    }
+    let newTotalString = newTotal.toFixed(2);
+    return newTotalString;
+  }
+
     render() {
+      // The method above is called inside the render function to regularly update the total cost, especially after any of the add to cart buttons are clicked on.
+      let totalCostNav = this.calculateTotalCost();
       return (
         <div className="Navbar">
             <div id="header">
               <div id="left-side">
-                <img id="logo-img" src="assets/logo/logo-01.svg"/>
+                <img id="logo-img" src={process.env.PUBLIC_URL + "/assets/logo/logo-01.svg"}/>
               </div>
               <div id="right-side">
                 <div id="menu-btns">
-                  <button class="txt menu-btn">PRODUCTS</button>
-                  <button class="txt menu-btn">CART</button>
+                  <button className="txt menu-btn">PRODUCTS</button>
+                  <button className="txt menu-btn">CART</button>
+                </div>
+                <div className="cartItems">
+                  <p id="numeroItems">{this.props.listCart.length} item(s)</p>
+                  <p id="totalCost">Total: $ {totalCostNav}</p>
                 </div>
                 <div>
                   <hr id="hr-line" />
                 </div>
                 <div>
-                  <p id="slogan-txt" class="txt">Our hand-made cinammon rolls</p>
+                  <p id="slogan-txt" className="txt">Our hand-made cinammon rolls</p>
                 </div>
               </div>
             </div>
--- solution-hw4/src/views/home/index.js
+++ solution-hw4/src/views/home/index.js
@@ -1,27 +1,56 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import Navbar from '../../Navbar';
-import RollCard from '../../RollCard';
+import Roll from '../../RollCard';
 
+// External Resources Citation: 
+// https://stackoverflow.com/questions/51974767/getting-broken-image-in-react-app --> helped me understand how to fix broken images issue
+// https://www.geeksforgeeks.org/reactjs-setstate/ --> helped me understand how to use setState
+// https://medium.com/@ozhanli/passing-data-from-child-to-parent-components-in-react-e347ea60b1bb --> helped me understand how to get data from a child component
+// https://stackoverflow.com/questions/26253351/correct-modification-of-state-arrays-in-react-js --> helped me understand how to add an element to a React state array
+
+// This file acts as the homepage for the website.
 class Homepage extends Component {
+  // We create a state property called listCart to create a list of all the orders made in the website.
+  constructor(props) {
+    super(props);
+    this.state = {
+        listCart: [],
+    };    
+  }
+
+  // The method below gets data from the child component, which in this case is Roll in RollCard.js. Each time the Roll component is called on, data returns back an object literal that contains the type of cinnamon roll order, the pack size, the glaze, and the calculated price.
+  // That object literal is then added to the listCart state property to keep track of orders.
+  // This method is called on each time the Roll component is called in render.
+  handleDataFromRollCard = (data) => {
+    // console.log(data);
+    this.setState( (prevState) => ({
+      listCart: [...prevState.listCart, data]
+    }), () => {
+      // console.log(this.state.listCart);
+    })
+  }
+
+  
   render() {
     return (
       <div className="Homepage">
         <div id="container">
           <header>
-            <title>PUI HW 1- Sapna Parihar</title>
+            <title>PUI HW 4- Sapna Parihar</title>
           </header>
-          <Navbar />
-            {/* <!-- croll list contains all the images, buttons, dropwdown menus, and text for each cinnamon roll --> */}
-            <div id="croll-list">
-              {/* <!-- each RollCard contains an image with the same content for the text, buttons, and dropdown menus --> */}
-              {/* <RollCard imageURL={"assets/products/original-cinnamon-roll.jpg"} type={"Original Cinnamon Roll"} basePrice={"$ 2.49"}/> */}
-              <RollCard imageURL={"assets/products/original-cinnamon-roll.jpg"} type={"Original Cinnamon Roll"} basePrice={"$ 2.49"}/>
-              <RollCard imageURL={"assets/products/apple-cinnamon-roll.jpg"} type={"Apple Cinnamon Roll"} basePrice={"$ 3.49"}/>
-              <RollCard imageURL={"assets/products/raisin-cinnamon-roll.jpg"} type={"Raisin Cinnamon Roll"} basePrice={"$ 2.99"}/>
-              <RollCard imageURL={"assets/products/walnut-cinnamon-roll.jpg"} type={"Walnut Cinnamon Roll"} basePrice={"$ 3.49"}/>
-              <RollCard imageURL={"assets/products/double-chocolate-cinnamon-roll.jpg"} type={"Double Chocolate Cinnamon Roll"} basePrice={"$ 3.99"}/>
-              <RollCard imageURL={"assets/products/strawberry-cinnamon-roll.jpg"} type={"Strawberry Cinnamon Roll"} basePrice={"$ 3.99"}/>
+          {/* Navbar contains the logo, the slogan, the menu buttons, the number of items, and the total cost. */}
+          {/* The listCart state property also gets passed through the Navbar component to calculate the total cost and the number of items. */}
+          <Navbar listCart={this.state.listCart} />
+          {/* <!-- croll list contains all the images, buttons, dropwdown menus, and text for each cinnamon roll --> */}
+          <div id="croll-list">
+            {/* <!-- Each Roll contains an image with the same content for the text, buttons, and dropdown menus --> */}
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/original-cinnamon-roll.jpg"} type={"Original Cinnamon Roll"} basePrice={"2.49"}/>
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/apple-cinnamon-roll.jpg"} type={"Apple Cinnamon Roll"} basePrice={"3.49"}/>
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/raisin-cinnamon-roll.jpg"} type={"Raisin Cinnamon Roll"} basePrice={"2.99"}/>
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/walnut-cinnamon-roll.jpg"} type={"Walnut Cinnamon Roll"} basePrice={"3.49"}/>
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/double-chocolate-cinnamon-roll.jpg"} type={"Double Chocolate Cinnamon Roll"} basePrice={"3.99"}/>
+            <Roll sendDataToHomepage={this.handleDataFromRollCard} imageURL={process.env.PUBLIC_URL + "/assets/products/strawberry-cinnamon-roll.jpg"} type={"Strawberry Cinnamon Roll"} basePrice={"3.99"}/>
           </div>
         </div>
       </div>
