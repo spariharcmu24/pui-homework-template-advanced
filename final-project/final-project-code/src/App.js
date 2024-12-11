c7f51a21c8776aa3a4ac312b1b4f0aed9cc71cfb
sp2418 <sparihar@andrew.cmu.edu>
2024-12-11 16:06:59 -0500

    made some changes to routing

--- final-project/final-project-code/src/App.js
+++ final-project/final-project-code/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Homepage from './pages/index.js';
 import JournalPage from './pages/journal.js';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import KeptSessions from './pages/keptsessions.js';
 // import AudioRecord from './pages/audiorecord.js';
 
@@ -12,14 +12,14 @@ class App extends Component {
   render() {
     // created routes to different pages in the app, including home page, journal page, and kept sessions page
     return (
-      <BrowserRouter basename={ROUTER_BASENAME}>
+      <Router basename={ROUTER_BASENAME}>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="journal" element={<JournalPage />} />
           {/* <Route path="audiorecord" element={<AudioRecord />} /> */}
           <Route path="keptsessions" element={<KeptSessions />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
